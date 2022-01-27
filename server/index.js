060426ef47f8fd43dc0a807d7d095663d77cb2c7
sonic <sonic@deprosoft.com>
2022-01-27 10:59:10 +0300

    Added static content and routing

--- server/index.js
+++ server/index.js
@@ -1,10 +1,32 @@
 const http = require('http');
+const fs = require('fs');
+const url = require('url');
+
 const port = 8080;
+
 const server = http.createServer((req, res) => {
+  var urlParts = url.parse(req.url);
+  var reqPath = urlParts.pathname;
   res.statusCode = 200;
-  const msg = 'Hello World!\n'
-  res.end(msg);
+  if (reqPath !== '/api') {
+    fs.readFile('api.txt', 'utf8' , (err, data) => {
+      if (err) {
+        console.error(err)
+        return
+      }
+      res.end(data);
+    })
+  } else {
+    fs.readFile('index.html', 'utf8' , (err, data) => {
+      if (err) {
+        console.error(err)
+        return
+      }
+      res.end(data);
+    })
+  }
 });
+
 server.listen(port, () => {
   console.log(`Server running on http://localhost:${port}/`);
 });
